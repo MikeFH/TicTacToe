45f783fbc4dcf47419a9daf68e9a42c87dfebe22
Michael <MikeFH@users.noreply.github.com>
2017-01-06 22:43:07 +0100

    Create IsEndOfGame method

--- TicTacToe/Program.fs
+++ TicTacToe/Program.fs
@@ -24,6 +24,16 @@ type Board(array:Symbol option[]) =
     let get x y =         
         array.[x + y * rowSize]
 
+    let getLines =
+        match array with
+        | [| a; b; c; d; e; f; g; h; i;|] -> [(a, b, c); (d, e, f); (g, h, i); (a, d, g); (b, e, h); (c, g, i); (a, e, i); (c, e, g)]
+        | _ -> failwith ""
+
+    let isLineWinForPlayer symbol line =
+        let (a, b, c) = line
+        not(Option.isNone a) && not(Option.isNone b) && not(Option.isNone c) && 
+            symbol = Option.get a && Option.get a = Option.get b && Option.get b = Option.get c
+
     let printPlayer(symbol:Symbol option) = 
         match symbol with
         | None -> ' '
@@ -35,6 +45,15 @@ type Board(array:Symbol option[]) =
         let newArray = Array.copy array
         Array.set newArray (move.X + move.Y * rowSize) (Some move.Symbol)
         new Board(newArray)
+
+    member this.IsPlayerWin symbol =
+        getLines.Any(fun line -> isLineWinForPlayer symbol line)
+
+    member this.IsEndOfGame = 
+        (this.IsPlayerWin Cross) || this.IsPlayerWin Circle || this.IsFull
+        
+    member this.IsFull =
+        array.All(fun e -> Option.isSome e)
 
     member this.Print() =
         printfn ""
@@ -53,19 +72,24 @@ type IPlayer =
     abstract member Symbol : Symbol with get
     abstract member GetMove : board:Board -> Move
 
-type ConsolePlayer(symbol) =
+type ConsolePlayer(symbol:Symbol) =
     let _symbol = symbol
 
-    interface IPlayer with
-        member val Symbol = _symbol
-        member this.GetMove(board) = 
-            printfn "Your move, Player %c (row,col) :" (_symbol.Print())
+    let rec getMoveInternal board = 
+        printfn "Your move, Player %c (row,col) :" (_symbol.Print())
+        try
             let moveConsole = Console.ReadLine().Trim().Split ','
             {
                 X = (Int32.Parse (moveConsole.[0].Trim())) - 1;
                 Y = (Int32.Parse (moveConsole.[1].Trim())) - 1;
                 Symbol = _symbol
             }
+        with
+            | _ -> getMoveInternal board
+
+    interface IPlayer with
+        member val Symbol = _symbol
+        member this.GetMove(board) = getMoveInternal board
             
 
 type Game(player1:IPlayer, player2:IPlayer) =
@@ -75,11 +99,12 @@ type Game(player1:IPlayer, player2:IPlayer) =
     let mutable currentPlayer = player1
     member this.Play = 
         board.Print()
-        while true do
+        while not board.IsEndOfGame do
             let move = currentPlayer.GetMove(board)
             board <- board.Move(move)
             board.Print()
             currentPlayer <- if currentPlayer = player1 then player2 else player1
+        board.Print()
         printfn "End game"
         
 
@@ -87,5 +112,5 @@ type Game(player1:IPlayer, player2:IPlayer) =
 let main argv = 
     let g = new Game(new ConsolePlayer(Circle), new ConsolePlayer(Cross))
     g.Play
-
+    ignore(Console.ReadLine())
     0 // retourne du code de sortie entier
